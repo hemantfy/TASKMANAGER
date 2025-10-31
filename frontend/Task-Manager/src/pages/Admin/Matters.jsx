f0dd1ee766380762e30e07423fe7c01abb044910
Hemant Kumar <ladhanihemant@gmail.com>
2025-10-31 15:04:33 +0530

    white screen fix

--- frontend/Task-Manager/src/pages/Admin/Matters.jsx
+++ frontend/Task-Manager/src/pages/Admin/Matters.jsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from "react";
 import {
-  LuAlertCircle,
+  LuCircleAlert,
   LuBriefcase,
   LuCalendarDays,
-  LuCheckBadge,
+  LuBadgeCheck,
   LuFileText,
   LuFolder,
   LuGavel,
